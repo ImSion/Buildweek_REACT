c45236db12ff2296059f79ad6706d2a0b37729e5
ImSion <Sion931@gmail.com>
2024-06-17 21:20:49 +0200

    badabum

--- Linkedin_React/src/App.jsx
+++ Linkedin_React/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import MyNav from './components/MyNav'
 import GetProfiles from './components/GetProfiles'
+import MyFooter from './components/MyFooter'
 
 function App() {
 
   return (
     <>
 
+    
     <GetProfiles />
     <MyNav />
+    <MyFooter />
 
     </>
   )
--- Linkedin_React/src/components/GetProfiles.jsx
+++ Linkedin_React/src/components/GetProfiles.jsx
@@ -3,19 +3,35 @@ import axios from '../modules/axios'
 
 export default function GetProfiles() {
 
-   axios.get('/profile/')
-  .then((response) => {
-    console.log("Data received:", response.data);
-  })
-  .catch((error) => {
-    if (error.response) {
-      console.error(`HTTP error: ${error.response.status}`);
-    } else if (error.request) {
-      console.error("Request error: No response received");
-    } else {
-      console.error("Error:", error.message);
-    }
-  });
+   axios.get('/profile/me')
+    .then((response) => {
+      console.log("Data received:", response.data);
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.error(`HTTP error: ${error.response.status}`);
+      } else if (error.request) {
+        console.error("Request error: No response received");
+      } else {
+        console.error("Error:", error.message);
+      }
+    });
+
+    axios.put('/profile/me')
+    .then((response) => {
+      console.log("Data received:", response.data);
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.error(`HTTP error: ${error.response.status}`);
+      } else if (error.request) {
+        console.error("Request error: No response received");
+      } else {
+        console.error("Error:", error.message);
+      }
+    });
+
+
   return (
     <div>
 
